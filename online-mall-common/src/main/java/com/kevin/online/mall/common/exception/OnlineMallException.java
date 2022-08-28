88a6dbc9c53ceb6c0a6645280a4a3e34eae4ed7c
kevin <yundan_wy@126.com>
2022-08-28 22:09:06 +0800

    项目初始化

---
+++ online-mall-common/src/main/java/com/kevin/online/mall/common/exception/OnlineMallException.java
@@ -0,0 +1,20 @@
+package com.kevin.online.mall.common.exception;
+
+/**
+ * 在线商城异常
+ *
+ * @author kevin
+ */
+public class OnlineMallException extends RuntimeException {
+
+
+  public OnlineMallException() {
+  }
+
+
+  public OnlineMallException(String message) {
+    super(message);
+  }
+
+
+}
